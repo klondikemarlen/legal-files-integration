0568fdd79d4ea23d6b18fb7fcedfbe2bcaa71a89
Marlen Brunner <klondikemarlen@gmail.com>
2022-06-27 11:59:13 -0700

    :wrench: Add linting via eslint.

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+	res.send("Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`)
+	console.log(`API listening at http://localhost:${port}`)
 })
