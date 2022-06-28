2814a73c5b6c14a5c891765e21741a72516a1efb
Marlen Brunner <klondikemarlen@gmail.com>
2022-06-28 09:47:53 -0700

    :fire: Drop missed console.log.

--- src/api/application-api.js
+++ src/api/application-api.js
@@ -1,9 +1,5 @@
 import axios from "axios"
 
-console.log(
-	"process.env.LEGAL_FILES_API_BASE_URL",
-	process.env.LEGAL_FILES_API_BASE_URL
-)
 const client = axios.create({
 	baseURL: process.env.LEGAL_FILES_API_BASE_URL,
 })
