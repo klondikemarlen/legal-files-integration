d9337fc68a2601360b39fc07bcedb37a052decaf
Marlen Brunner <klondikemarlen@gmail.com>
2022-06-28 09:08:58 -0700

    :art: Add method to print list of API routes.

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,20 @@ import formAController from "@/controllers/form-a-controller"
 
 const router = Router()
 
-router.get("/", (req, res) => {
-	res.send("Legal Files Integration API.")
-})
 router.route("/form-a").post(formAController.postInjestForm)
+
+// Return list of routes.
+router.get("/", (req, res) => {
+	const data = []
+	router.stack.forEach((element) => {
+		const route = element.route
+		if (route?.path) {
+			const methods = Object.keys(route.methods).sort().join(",")
+			data.push(`${route.path} - ${methods}`)
+		}
+	})
+
+	res.send("Legal Files Integration API Routes<br><br>" + data.join("<br>"))
+})
 
 export default router
