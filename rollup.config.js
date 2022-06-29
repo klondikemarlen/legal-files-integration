65c694ba8cede970370f30226189f3d8166bc059
Marlen Brunner <klondikemarlen@gmail.com>
2022-06-29 15:54:04 -0700

    :construction: Add rollup config that sort of works.
    It doesn't compile the models/ folder yet. I might just switch to webpack.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import path from "path"
 
 import babel from "@rollup/plugin-babel"
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
+import nodeResolve from "@rollup/plugin-node-resolve"
+import nodeGlobals from "rollup-plugin-node-globals"
 
 export default {
 	input: "./server.js",
@@ -8,10 +12,22 @@ export default {
 		file: "./dist/server-bundle.js",
 		format: "cjs",
 	},
+	external: ["fs", "path", "sequelize"],
 	plugins: [
 		babel({
+			include: ["**/*.js"],
 			exclude: "node_modules/**",
 			configFile: path.resolve(__dirname, "babel.config.json"),
+			babelHelpers: "bundled",
 		}),
+		nodeResolve({
+			moduleDirectories: ["node_modules"],
+			preferBuiltins: true,
+		}),
+		json(),
+		commonjs({
+			ignoreDynamicRequires: true,
+		}),
+		nodeGlobals(),
 	],
 }
