f62740f343d43256ef1bc701419ddb1fa2d7511f
Marlen Brunner <klondikemarlen@gmail.com>
2022-06-30 09:25:05 -0700

    :sparkles: Allow truncating all test data.
    Sloooow, but avoids foreign key constraint issues.

--- tests/hooks.js
+++ tests/hooks.js
@@ -2,10 +2,24 @@ const td = require("testdouble")
 
 import db from "@/models"
 
+// Sloooow, but avoids foreign key constraint issues.
+function truncateAllTables() {
+	return Promise.all(
+		Object.values(db.sequelize.models).map((model) => {
+			return model
+				.findAll()
+				.then((instances) => {
+					return Promise.all(instances.map((instance) => instance.destroy()))
+				})
+				.catch((error) => console.error(error))
+		})
+	)
+}
+
 // reset testdouble mocks after each test.
 exports.mochaHooks = {
 	beforeEach() {
-		return db.sequelize.sync({ force: true })
+		return truncateAllTables()
 	},
 	afterEach(done) {
 		td.reset()
